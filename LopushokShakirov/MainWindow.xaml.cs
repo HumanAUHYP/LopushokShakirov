0655a9b6e16377b6271a22f40942731b7a7f2e7c
Human <ainur.hak2004@gmail.com>
2023-02-10 19:32:09 +0300

    Добавьте файлы проекта.

---
+++ LopushokShakirov/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LopushokShakirov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.Navigate(new Pages.ProductListPage());
+            BackForwardVisibility();
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (mainFrame.CanGoBack) mainFrame.GoBack();
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (mainFrame.CanGoForward) mainFrame.GoForward();
+        }
+
+        public void BackForwardVisibility()
+        {
+            if (mainFrame.CanGoBack) btnBack.Visibility = Visibility.Visible;
+            if (mainFrame.CanGoForward) btnForward.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ LopushokShakirov/Pages/ProductListPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LopushokShakirov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListPage.xaml
+    /// </summary>
+    public partial class ProductListPage : Page
+    {
+        public List<Pagin> Pages = new List<Pagin>();
+        public ProductListPage()
+        {
+            InitializeComponent();
+            Pages.Add(new Pagin { Title = "1"});
+            Pages.Add(new Pagin { Title = "2" });
+            Pages.Add(new Pagin { Title = "3" });
+            lvPagination.ItemsSource = Pages;
+        }
+    }
+    public class Pagin
+    {
+        public string Title { get; set; }
+    }
+
+}
